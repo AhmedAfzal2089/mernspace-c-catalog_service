27da6500928094938e02ea4c87e96ecbcd74c922
AhmedAfzal2089 <ameafzal@gmail.com>
2025-07-31 13:29:09 +0500

    add authorization (only admin)

--- src/common/types/index.ts
+++ src/common/types/index.ts
@@ -1,3 +1,9 @@
+import { Request } from "express";
+
 export type AuthCookie = {
     accessToken: string;
 };
+
+export interface AuthRequest extends Request {
+    auth: { sub: string; role: string; id?: string }; // the data we are sending in token
+}
