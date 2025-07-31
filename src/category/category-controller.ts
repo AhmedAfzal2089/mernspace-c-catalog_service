7a40d88fd9ff6b269fba3fef05a9a3ab412738be
AhmedAfzal2089 <ameafzal@gmail.com>
2025-08-01 02:01:43 +0500

    complete CRU of category

--- src/category/category-controller.ts
+++ src/category/category-controller.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { validationResult } from "express-validator";
 import createHttpError from "http-errors";
-import { Category } from "./category-types";
+import { Category, PriceConfiguration } from "./category-types";
 import { CategoryService } from "./category-service";
 import { Logger } from "winston";
 
@@ -11,7 +11,11 @@ export class CategoryController {
         private logger: Logger,
     ) {
         //when create method is called , it will get its proper contexts
+
         this.create = this.create.bind(this);
+        this.update = this.update.bind(this);
+        this.index = this.index.bind(this);
+        this.getOne = this.getOne.bind(this);
     }
     async create(req: Request, res: Response, next: NextFunction) {
         const result = validationResult(req);
@@ -30,5 +34,52 @@ export class CategoryController {
             id: category._id,
         });
         res.status(201).json({ id: category._id });
+    }
+    async index(req: Request, res: Response) {
+        const categories = await this.categoryService.getAll();
+        this.logger.info(`Getting categories list`);
+        res.json(categories);
+    }
+    async getOne(req: Request, res: Response, next: NextFunction) {
+        const { categoryId } = req.params;
+        const category = await this.categoryService.getOne(categoryId);
+        if (!category) {
+            return next(createHttpError(404, "Category not found"));
+        }
+        this.logger.info(`Getting category`, { id: category._id });
+        res.json(category);
+    }
+    async update(req: Request, res: Response, next: NextFunction) {
+        const result = validationResult(req);
+        if (!result.isEmpty()) {
+            return next(createHttpError(400, result.array()[0].msg as string));
+        }
+
+        const categoryId = req.params.id;
+        const updateData = req.body as Partial<Category>;
+
+        // Check if category exists
+        const existingCategory = await this.categoryService.getOne(categoryId);
+
+        if (!existingCategory) {
+            return next(createHttpError(404, "Category not found"));
+        }
+        if (updateData.priceConfiguration) {
+            const existingConfig = existingCategory.priceConfiguration ?? {};
+
+            const mergedConfig: PriceConfiguration = {
+                ...existingConfig,
+                ...updateData.priceConfiguration,
+            };
+
+            updateData.priceConfiguration = mergedConfig;
+        }
+        await this.categoryService.update(categoryId, updateData);
+
+        this.logger.info(`Updated category`, { id: categoryId });
+
+        res.json({
+            id: categoryId,
+        });
     }
 }
--- src/category/category-service.ts
+++ src/category/category-service.ts
@@ -6,4 +6,27 @@ export class CategoryService {
         const newCategory = new CategoryModel(category);
         return newCategory.save();
     }
+    async update(
+        categoryId: string,
+        updateData: Partial<Category>,
+    ): Promise<Category | null> {
+        return await CategoryModel.findByIdAndUpdate(
+            categoryId,
+            {
+                $set: updateData, // It replaces only the provided fields, without affecting the rest.
+            },
+            {
+                new: true, //Mongoose will return the updated document instead.
+            },
+        );
+    }
+    async getOne(categoryId: string) {
+        const category = await CategoryModel.findOne({
+            _id: categoryId,
+        }).lean();
+        return category;
+    }
+    async getAll() {
+        return await CategoryModel.find();
+    }
 }
