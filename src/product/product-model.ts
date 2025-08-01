7657bf82c3a2a54882a6dfb36a3d0efcec0ad824
AhmedAfzal2089 <ameafzal@gmail.com>
2025-08-01 16:35:05 +0500

    add product model

---
+++ src/product/product-model.ts
@@ -0,0 +1,60 @@
+import mongoose from "mongoose";
+
+const attributeValueSchema = new mongoose.Schema({
+    name: {
+        type: String,
+    },
+    value: {
+        type: mongoose.Schema.Types.Mixed,
+    },
+});
+
+const priceConfigurationSchema = new mongoose.Schema({
+    priceType: {
+        type: String,
+        enum: ["base", "aditional"],
+        required: true,
+    },
+    availableOptions: {
+        type: Map, // object within an object
+        of: Number,
+    },
+});
+
+const productSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        image: {
+            type: String,
+            required: true,
+        },
+        priceConfiguration: {
+            type: Map,
+            of: priceConfigurationSchema,
+        },
+        attributes: [attributeValueSchema],
+        tenantId: {
+            type: String, //to convert string to number is easy (btw in auth service tenId is string)
+            required: true,
+        },
+        categoryId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Category", // giving reference of model ,. bcz categoryId belong to any Category
+        },
+        isPublish: {
+            type: Boolean,
+            required: false,
+            default: false,
+        },
+    },
+    { timestamps: true },
+);
+
+export default mongoose.model("Product", productSchema);
